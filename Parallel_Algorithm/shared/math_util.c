0e45d122f5f9c07bab37e93afb5584e977b23d81
Jiawei Zhuang <jiaweizhuang@g.harvard.edu>
2017-04-29 05:04:03 +0000

    compile bug fix

--- Parallel_Algorithm/shared/math_util.c
+++ Parallel_Algorithm/shared/math_util.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include "math_util.h"
 
 // square of the distance between x1[N_features] and x2[N_features]
@@ -11,11 +12,11 @@ float distance(int N_features,float *x1,float *x2){
 float correlation(int N_features,float *x,float *y){
    float xsum=0.0,ysum=0.0,xysum=0.0,xsqr_sum=0.0,ysqr_sum=0.0;
    for (int j = 0; j<N_features; j++) {
-            xsum = xsum + x[i];
-            ysum = ysum + y[i];
-            xysum = xysum + x[i] * y[i];
-            xsqr_sum = xsqr_sum + x[i] * x[i];
-            ysqr_sum = ysqr_sum + y[i] * y[i];
+            xsum = xsum + x[j];
+            ysum = ysum + y[j];
+            xysum = xysum + x[j] * y[j];
+            xsqr_sum = xsqr_sum + x[j] * x[j];
+            ysqr_sum = ysqr_sum + y[j] * y[j];
     }
 
     float num = ((N_features * xysum) - (xsum * ysum));
